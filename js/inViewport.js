ba52c5d3b13855dfcc414d10d79bb1aef0635c23
Joe Rickerby <joerick@mac.com>
2016-03-15 14:25:26 +0000

    Only activate scroll handlers when in middle 1/3 of page

--- js/inViewport.js
+++ js/inViewport.js
@@ -1,13 +1,18 @@
 /*! inViewport 0.0.1
  *  jQuery plugin by Moob
+ *  modified by joerick to activate only in the middle 1/3 of the page
  * ========================
  *  (requires jQuery)
  */
 (function ($) {
+    'use strict';
+    var detection_box_start_y = 0;
+    var detection_box_end_y = 0;
 
-    var vph=0;
-    function getViewportDimensions(){
-        vph = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    function getViewportDimensions() {
+        var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        detection_box_start_y = viewportHeight * 1/3;
+        detection_box_end_y = viewportHeight * 2/3;
     }
     getViewportDimensions();
     //on resize/scroll
@@ -25,10 +30,8 @@
                 checkInView();
             });
             function checkInView(){
-                var rect = el[0].getBoundingClientRect(),
-                    t = rect.top,
-                    b = rect.bottom;
-                if(t<vph && b>0){
+                var rect = el[0].getBoundingClientRect();
+                if(rect.top < detection_box_end_y && rect.bottom > detection_box_start_y) {
                     if(!inviewalreadycalled){
                         whenInView.call(el);
                         inviewalreadycalled = true;
@@ -45,5 +48,5 @@
             //initial check
             checkInView();
         });
-    }
+    };
 }(jQuery));
